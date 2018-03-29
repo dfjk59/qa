d84e748bb774623c41538720bcb5552e5ae0588a
dfjk59 <dfjk59@126.com>
2018-03-29 18:32:17 +0800

    迁移数据库

--- main/models.py
+++ main/models.py
@@ -2,18 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=70)
+    title = models.CharField(max_length=70) # 文章标题
     body = models.TextField()
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)#添加级联
+    excerpt = models.CharField(max_length=200, blank=True) # 文章摘要
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 添加级联
     tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)#添加级联
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # 添加级联
