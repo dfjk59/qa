7dc8e71ca937fdb22e91e4735a151fbbd0f23bf6
dfjk59 <dfjk59@126.com>
2018-03-29 18:38:43 +0800

    增加视图

--- main/models.py
+++ main/models.py
@@ -7,17 +7,26 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=70) # 文章标题
+    title = models.CharField(max_length=70)  # 文章标题
     body = models.TextField()
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True) # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)  # 文章摘要
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 添加级联
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # 添加级联
+
+    def __str__(self):
+        return self.title
