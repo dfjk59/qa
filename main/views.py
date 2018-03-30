0652ece959b7a5e312dab9ed100fe8c4be5ff7dd
dfjk59 <dfjk59@126.com>
2018-03-30 16:43:34 +0800

    完善分类功能

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 import markdown
 # Create your views here.
 
@@ -24,4 +24,10 @@ def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
                                     ).order_by('-created_time')
+    return render(request, 'index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'index.html', context={'post_list': post_list})
