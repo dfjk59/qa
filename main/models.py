4e9d874bfcc4df4517c0b8003dc9baacd2f1b932
dfjk59 <dfjk59@126.com>
2018-04-02 19:56:13 +0800

    完善阅读量

--- main/models.py
+++ main/models.py
@@ -28,12 +28,17 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 添加级联
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # 添加级联
+    views = models.PositiveIntegerField(default=0)  # 阅读数
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     class Meta:
         ordering = ['-created_time']
